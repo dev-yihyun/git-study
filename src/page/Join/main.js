6086b86a437a1a679c2697aa8f753a61aa85549e
dev-yihyun <dev_yihyun@naver.com>
2025-01-09 18:12:09 +0900

    [feat] Join 페이지 레이아웃 수정
    [description]
    - Page 레이아웃 추가
    - FlexBox 레이아웃 추가

--- src/page/Join/main.js
+++ src/page/Join/main.js
@@ -1,20 +1,28 @@
 import Button from "../../component/Button/Button";
 import Card from "../../component/Card/Card";
 import Input from "../../component/Input/Input";
+import FlexBox from "../../component/Layout/FlexBox";
+import Page from "../../component/Layout/Page";
 
 function Join() {
     return (
         <>
-            <Card>
-                <Input placeholder="ID" />
-                <Button>Check ID</Button>
-                <Input placeholder="PW" />
-                <Input placeholder="PW" />
-                <Input placeholder="NAME" />
-                <Input placeholder="PHONE" />
-                <Input placeholder="EMAIL" />
-                <Button>Sign up</Button>
-            </Card>
+            <Page>
+                <FlexBox>
+                    <Card>
+                        <FlexBox gap="10px">
+                            <Input placeholder="ID" />
+                            <Button>Check ID</Button>
+                            <Input placeholder="PW" />
+                            <Input placeholder="PW" />
+                            <Input placeholder="NAME" />
+                            <Input placeholder="PHONE" />
+                            <Input placeholder="EMAIL" />
+                            <Button>Sign up</Button>
+                        </FlexBox>
+                    </Card>
+                </FlexBox>
+            </Page>
         </>
     );
 }
